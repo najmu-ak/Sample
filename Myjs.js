4118272f0f3c8e9ba4931b8169fc93a084ed6747
najmuak <aknajmudeen104@gmail.com>
2024-08-14 12:29:06 +0530

    create a js page

---
+++ Myjs.js
@@ -0,0 +1,104 @@
+/// function
+function samsung() {
+    console.log("This is samsung")
+}
+
+function iphone() {
+    console.log("This is iphone")
+}
+
+function realme() {
+    console.log("This is realme")
+}
+
+function redmi() {
+    console.log("This is redmi")
+}
+
+function ABC() {
+    console.log("This is Abc function")
+}
+
+redmi()
+samsung()
+iphone()
+realme()
+ABC()
+
+/// workout 
+
+var a = 10;
+var b = 20;
+
+function add() {
+    console.log(a + b)
+}
+
+add()
+/// function workout
+var factor = "Sivakarthik";
+var fplayer = "Dhoni";
+var fmovie = "Don";
+
+function fav() {
+    console.log("factor is:" + factor);
+    console.log("fplayer is:" + fplayer);
+    console.log("fmovie is:" + fmovie);
+
+}
+
+fav()
+
+/// Workout
+
+function signal() {
+    var traffic = "green";
+
+    if (traffic == "green") {
+        console.log("Go")
+    }
+
+    else if (traffic == "yellow") {
+        console.log("Get ready")
+    }
+
+    else {
+        console.log("Stop")
+    }
+}
+signal()
+
+/// Function with conditional statments				
+
+function rankcard() {
+
+    var score = 101;
+
+    if (score >= 35 && score < 70) {
+        console.log("You need to improve")
+    }
+
+    else if (score >= 70 && score < 95) {
+        console.log("Wow you have a good marks")
+    }
+
+    else if (score >= 95 && score <= 100)
+        console.log("What a Mark....")
+
+    else {
+        console.log("You have fail")
+    }
+
+}
+rankcard()
+
+/// For loop 
+
+for (let i = 1; i <= 10; i += 1) {
+    console.log(i + "x2=" + i * 2)
+}
+
+// var demo =document.getElementById("demo");
+// console.log(demo);
+// console.log(demo.textContent);
+
